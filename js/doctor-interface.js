09b930b361db22d38f8a6ee4839d3a9fd869152b
Clifford Grimmell <agrimmell2@gmail.com>
2017-05-03 08:10:04 -0700

    format index.html file

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,26 +4,26 @@ var showDoctor = function(doctors) {
   for (var i = 0; i < doctors.length; i += 1) {
     try {
       $("#results").append(
-          '<div class="panel panel-default">' +
-            '<div class="panel-header" role="tab" + id="header' + i + '">' +
-              '<h3 class="panel-title">' +
-                  doctors[i].profile.first_name + ' ' + doctors[i].profile.last_name +
-              '</h3>' +
-            '</div>' +
-            '<div class="panel-body">' +
-              '<img src=' + doctors[i].profile.image_url + '>' +
-              '<h4>Specialty</h4>' +
-              doctors[i].specialties[0].name +
-              '<h4>Practice</h4>' +
-                doctors[i].practices[0].name +
-              '<h4>Location</h4>' +
-                doctors[i].practices[0].visit_address.street + ', ' + doctors[i].practices[0].visit_address.city doctors[i].practices[0].visit_address.state + ', ' + doctors[i].practices[0].visit_address.zip +
-              '<h4>NPI</h4>' +
-                doctors[i].npi +
-              '<h4>Bio</h4>' +
-                doctors[i].profile.bio +
-            '</div>' +
-          '</div>'
+          `<div class="panel panel-default">
+            <div class="panel-header" role="tab"  id="header+i">
+              <h3 class="panel-title">
+                doctors[i].profile.first_name  doctors[i].profile.last_name
+              </h3>
+            </div>
+            <div class="panel-body">
+              <img src='doctors[i].profile.image_url'>
+              <h4>Specialty</h4>
+              doctors[i].specialties[0].name
+              <h4>Practice</h4>
+                doctors[i].practices[0].name
+              <h4>Location</h4>
+                doctors[i].practices[0].visit_address.street ,  doctors[i].practices[0].visit_address.city doctors[i].practices[0].visit_address.state ,  doctors[i].practices[0].visit_address.zip
+              <h4>NPI</h4>
+                doctors[i].npi
+              <h4>Bio</h4>
+                doctors[i].profile.bio
+            </div>
+          </div>`
     } catch(error) {
       console.log("Failed to get Doctor");
     }
@@ -35,7 +35,6 @@ $(document).ready(function() {
   var doctorObject = new Doctor();
 
   $('#search').submit(function() {
-    $("#loading").show();
     event.preventDefault();
     $('#results').empty();
     var firstName = $('#firstName').val().replace(" ", "-");
